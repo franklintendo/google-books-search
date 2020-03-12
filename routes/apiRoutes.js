4c6a33a3ba0b6ae556a7581abfb60241a75b166e
Frank Moran <info@francisfrankmoran.com>
2020-03-12 02:51:15 -0400

    abstraction

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const axios = require("axios");
 
 router.get("/books/:id", (req,res) => {
-  const queryUrl = `https://www.googleapis.com/books/v1/volumes?q=${req.params.id}&key=AIzaSyAWlXVYB9JtE-sL59dE0Qw_WEz67I0T9Kk`;
+  const queryUrl = `https://www.googleapis.com/books/v1/volumes?q=${req.params.id}&key=${process.env.API_KEY}`;
 
   axios.get(queryUrl)
   .then(response => res.send(response.data))
@@ -10,7 +10,7 @@ router.get("/books/:id", (req,res) => {
 });
 
 router.get("/books", (req,res) => {
-  res.send("hi");
+  res.send("key: " + process.env.API_KEY);
 });
 
 
